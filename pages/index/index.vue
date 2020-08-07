a845a67ff28eea1bc3f956d32746220fe213ae99
zhangkai35 <zhangkai35@58.com>
2020-08-08 01:47:17 +0800

    初次上传

---
+++ pages/index/index.vue
@@ -0,0 +1,31 @@
+<template>
+	<view class="container">
+		<view class="intro">本项目已包含uni ui组件，无需import和注册，可直接使用。在代码区键入字母u，即可通过代码助手列出所有可用组件。光标置于组件名称处按F1，即可查看组件文档。</view>
+		<text class="intro">详见：</text>
+		<uni-link :href="href" :text="href"></uni-link>
+		<navigator url="../login/login">
+			<button type="default">跳转到登录页</button>
+		</navigator>
+	</view>
+</template>
+
+<script>
+	export default {
+		data() {
+			return {
+				href: 'https://uniapp.dcloud.io/component/README?id=uniui'
+			}
+		},
+		methods: {
+
+		}
+	}
+</script>
+
+<style>
+	.container {
+		padding: 20px;
+		font-size: 14px;
+		line-height: 24px;
+	}
+</style>
---
+++ pages/login/login.vue
@@ -0,0 +1,22 @@
+<template>
+	<view>
+		登录页
+	</view>
+</template>
+
+<script>
+	export default {
+		data() {
+			return {
+				
+			}
+		},
+		methods: {
+			
+		}
+	}
+</script>
+
+<style>
+
+</style>
